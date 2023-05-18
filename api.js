1810465eff96f1bad8feadbe9159fb06fd53dbb2
Saurabh Sinha <sinhasaurabh079@gmail.com>
2023-05-18 20:26:12 +0530

    Create api.js

---
+++ api.js
@@ -0,0 +1,6 @@
+module.exports = {
+  getRank: function(leetcodeId) {
+    // Make an API call to LeetCode to get the number of questions and contest rank for the specified user.
+    // Return the data in a format that can be displayed on the website.
+  }
+};
